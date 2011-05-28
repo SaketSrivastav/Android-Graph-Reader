71dad6c0a55cde81064ba80ccf4e4f3b885459be
SaketSrivastav <saketsrivastav79gmail.com>
2011-05-28 14:15:17 +0530

    Project First Commit

---
+++ src/com/android/saket/graph/creator/CreateGraphUI.java
@@ -0,0 +1,13 @@
+package com.android.saket.graph.creator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CreateGraphUI extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
